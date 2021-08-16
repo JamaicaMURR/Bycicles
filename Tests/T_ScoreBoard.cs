2c21ad2bf3427744d00f3cdbd83b596b4eb10e88
murr123yandex.ru <murr123yandex.ru@outlook.com>
2021-08-17 02:03:06 +0300

    ScoreBoard tested

---
+++ Tests/T_ScoreBoard.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bycicles;
+
+namespace Tests
+{
+    [TestClass]
+    public class T_ScoreBoard
+    {
+        [TestMethod]
+        public void TryToInsert_test()
+        {
+            ScoreBoard<string, int> sb = new ScoreBoard<string, int>(3);
+
+            sb.TryToInsert("worst", 34);
+            sb.TryToInsert("best", 100);
+            sb.TryToInsert("mid", 56);
+
+            Assert.AreEqual(34, sb[2].Item2);
+            Assert.AreEqual(56, sb[1].Item2);
+            Assert.AreEqual(100, sb[0].Item2);
+
+            sb.TryToInsert("new best", 120);
+
+            Assert.AreEqual(56, sb[2].Item2);
+            Assert.AreEqual(100, sb[1].Item2);
+            Assert.AreEqual(120, sb[0].Item2);
+
+            sb.TryToInsert("new worst", 3);
+
+            Assert.AreEqual(56, sb[2].Item2);
+            Assert.AreEqual(100, sb[1].Item2);
+            Assert.AreEqual(120, sb[0].Item2);
+
+            sb.TryToInsert("new mid", 102);
+
+            Assert.AreEqual(100, sb[2].Item2);
+            Assert.AreEqual(102, sb[1].Item2);
+            Assert.AreEqual(120, sb[0].Item2);
+        }
+    }
+}
